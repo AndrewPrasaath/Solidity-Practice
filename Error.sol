6b1c5178dc032e9ee6777c999d143d13391e1205
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-03-17 07:05:02 +0530

    Error Handling

---
+++ Error.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+error NotEnoughPlayers(uint256 _participants);
+
+contract ErrorHandling {
+    string public game;
+    uint256 public players;
+    uint256 public participants;
+
+    constructor(string memory _game, uint256 _players) {
+        game = _game;
+        players = _players;
+    }
+
+    // function to check enough participants count using require
+    function countCheck(uint256 _participants) external {
+        require(_participants >= players, "Not enough players to participate!");
+        participants = _participants;
+        assert(players > 0); // players should never be zero
+    }
+
+    // function to check enough participants count using revert with custom error
+    function countCheckWithRevert(uint256 _participants) external {
+        if (_participants < players) {
+            revert NotEnoughPlayers(_participants);
+        }
+        participants = _participants;
+        assert(players > 0); // players should never be zero
+    }
+}
